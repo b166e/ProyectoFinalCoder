2515629d474e494c49c196888e162cd159d8b83c
b166er <brunojbarrera@hotmail.com>
2024-09-04 21:31:36 -0300

    Primer commit de carga

---
+++ AppCoder/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+from AppCoder import views
+
+urlpatterns = [
+    path('', views.inicio, name="Inicio"),
+    path('cursos/', views.cursos, name="Cursos"),
+    path('profesores/', views.profesores, name="Profesores"),
+    path('estudiantes/', views.estudiantes, name="Estudiantes"),
+    path('entregables/', views.entregables, name="Entregables"),
+    path('buscar/', views.buscar),
+    path('leerProfesores/', views.leerProfesores, name="LeerProfesores"),
+    path('eliminarProfesor/<str:profesor_nombre>/', views.eliminarProfesor, name="EliminarProfesor"),
+    path('editarProfesor/<str:profesor_nombre>/', views.editarProfesor, name="EditarProfesor"),
+    
+    path('curso/list/', views.CursoList.as_view(), name='List'),
+    path('curso/<int:pk>/', views.CursoDetalle.as_view(), name='Detail'),
+    path('curso/nuevo/', views.CursoCreacion.as_view(), name='New'),
+    path('curso/editar/<int:pk>/', views.CursoUpdate.as_view(), name='Edit'),
+    path('curso/borrar/<int:pk>/', views.CursoDelete.as_view(), name='Delete'),
+    
+    path('login/', views.login_request, name='Login'),
+    path('register/', views.register, name='Register'),
+    path('logout/', LogoutView.as_view(), name='Logout'), 
+    path('editarPerfil/', views.editarPerfil, name="EditarPerfil"),
+    path('agregarAvatar/', views.agregarAvatar, name="AgregarAvatar"),
+]
---
+++ README.md
@@ -0,0 +1,5 @@
+# ProyectoCoder Bruno Brrera
+
+- Entrega Final del curso de Coderhouse Comision 57820 
+
+Para esta entrega utilicé el html de la entrega del proyecto del curso de Desarrollo Web aplicando los conocimientos aprendidos en de Django. 
